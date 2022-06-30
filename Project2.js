8cc5f00342f21b96a2a770a3c68c4e95fef0002a
eddievonfleet <75598831+eddievonfleet@users.noreply.github.com>
2022-06-30 01:28:22 -0500

    changes

--- Project2.js
+++ Project2.js
@@ -2,7 +2,7 @@ const toggle = () => {
   var x = document.getElementById("links");
   if (x.style.display === "none") {
     x.style.display = "flex";
-    x.style.transition = "0.4s ease"
+
   } else {
     x.style.display = "none";
   }
